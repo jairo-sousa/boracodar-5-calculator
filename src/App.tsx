976d2d0e1d6f7f238d2212bdd95b37f7084605b1
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-02-06 19:29:34 -0300

    style: set main App main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Box, Flex } from "@chakra-ui/react";
+
+import { Screen } from "./components/Screen";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>IN PRODUCTION</h1>
-		</div>
+		<Flex
+			className="App"
+			w="35.6rem"
+			h="56.6rem"
+			direction="column"
+			align="center"
+			justify="center"
+			backgroundColor="#2D2A37"
+			borderRadius="4.8rem"
+		>
+			<Screen />
+			<Box as="section">keys</Box>
+		</Flex>
 	);
 }
 
---
+++ src/components/Screen.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export function Screen() {
+	return (
+		<Box as="section">
+			<Box>Screen</Box>
+		</Box>
+	);
+}
