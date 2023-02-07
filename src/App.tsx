c85602333097cc8a37636c42e4f9e6e93c42a5a4
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-02-06 21:20:32 -0300

    style: set Screen

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,11 @@ function App() {
 			h="56.6rem"
 			direction="column"
 			align="center"
-			justify="center"
+			justify="space-between"
 			backgroundColor="#2D2A37"
 			borderRadius="4.8rem"
+			p="5.4rem 3.4rem 3.2rem"
+			gap="2.6rem"
 		>
 			<Screen />
 			<Box as="section">keys</Box>
--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,9 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Flex, Img, Text } from "@chakra-ui/react";
 
 export function Screen() {
 	return (
-		<Box as="section">
-			<Box>Screen</Box>
-		</Box>
+		<Flex as="section" direction="column" w="100%" px="2rem" gap="0.8rem">
+			<Text color="#6B6B6B" fontSize="2rem" w="100%" align="end">
+				1 + 1
+			</Text>
+			<Flex justify="space-between">
+				<Img src="https://raw.githubusercontent.com/jairo-sousa/boracodar-5-calculator/main/.github/equals.svg" />
+				<Text fontSize="3.6rem">2</Text>
+			</Flex>
+		</Flex>
 	);
 }
